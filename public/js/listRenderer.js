e53c4e1998029980f4a765cb4f16ec5a43ad841a
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2014-12-20 18:54:23 +0100

    commit

---
+++ public/js/listRenderer.js
@@ -0,0 +1,54 @@
+var SG = SG || {};
+
+SG.listRenderer = {
+    selectors: {
+        container: 'container'
+    },
+
+    init: function () {
+
+    },
+
+    render: function () {
+        var personsContainer = document.getElementById(this.selectors.container);
+        var persons = this.getFixtures(10);
+
+        var nodes = [];
+        for (var i = 0; i < persons.length; i++) {
+            var person = persons[i];
+
+            nodes.push(this.createNode(person));
+        }
+
+        personsContainer.innerHTML = nodes.join('');
+    },
+
+    createNode: function(person) {
+        return this.getTemplate()
+            .replace('%%_id_%%', person.id)
+            .replace('%%_avatar_%%', person.img)
+            .replace('%%_name_%%', person.name);
+    },
+
+    getFixtures: function (amount) {
+        var fixtures = [];
+        for (var i = 1; i <= amount; i++) {
+            fixtures.push({
+                id: i,
+                name: 'King George ' + i,
+                img: 'public/img/' + i + '.png'
+            });
+        }
+
+        return fixtures;
+    },
+
+    getTemplate: function () {
+        return String.concat(
+            '<div class="person" id="%%_id_%%">',
+                '<img class="photo" src="%%_avatar_%%">',
+                '<p class="name">%%_name_%%</a>',
+            '</div>'
+        );
+    }
+};
