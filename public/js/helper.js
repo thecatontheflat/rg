94ddf0af03799a34156a460be4141e3620d99924
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2014-12-21 22:44:34 +0100

    Documented methods

--- public/js/helper.js
+++ public/js/helper.js
@@ -4,10 +4,20 @@ RG.helper = {
     template: '',
     names: ["Sullivan Fuller","Whitfield Macdonald","Melody Douglas","Berger Keith","Mcgowan Rodriguez","Wilkerson Pearson","Farrell Kelley","Helene Whitney","Kim Brewer","Daugherty Franklin","Simon King","Mcintosh Harding","Sophie Morton","Chaney Boyd","Regina Russo","Harvey Foreman","Mclean Dean","Virgie Ingram","Ebony Mckay","Tracie Tillman","Williamson Ryan","Rogers Woodard","Rosalinda Simpson","Eula Gibson","Watts Bonner","Tamera Henderson","Esperanza Crosby","Atkins Bailey","Sadie Rios","Mia Caldwell","Elvia Kim","Glenna Carver","Galloway Manning","Kramer Collins","Daphne Grimes","Cruz Dunlap","Bertie Pennington","Rosanna Blanchard","Cash Drake","Maynard Conner","Barnes Kerr","Bright Carpenter","Mitchell Lopez","Wilkins Deleon","Day Bright","Roy Frye","Lawrence Goodman","Katelyn Dominguez","Harriet Foster","Latonya Santos","Horne Mcleod","Mayra Stout","Meghan Benton","Jerri Ramsey","Dunn Bush","Rosalie Vance","Warner Whitley","Wolfe Waller","Moss Russell","Olson Gomez","Russo Mendez","Lorie Haney","Payne Snider","Leblanc Gonzales","Ginger Alvarez","Dorthy Gould","Oconnor Williamson","Silva Garrison","Virginia Leon","Blair Bell","Wynn Sharpe","Ester Pate","Phelps Dennis","Leanna Gibbs","Viola Banks","Buck Miranda","Parker Daugherty","Dominguez Howe","Chris Molina","Wade Battle","Rhodes Leblanc","Alba Cain","Parks Hooper","Yvonne Summers","Sears Glenn","Fannie Park","Vega Randall","Gay Sullivan","Moran Irwin","Rush Cross","Kent Valentine","Amanda Wright","Palmer Franks","Lucinda Malone","Heath Perry","Hampton Sandoval","Hayden Kemp","Small Slater","Woods Walter","Compton Bass","Harrell Oneill","Nelson Workman","Bass Marsh","Coleen Montoya","Hopkins Joyner","Deena Kirkland","Cassandra Sellers","Johnnie Finley","Dudley Benjamin","Stokes Vincent","Kathie Murphy","Jody Avila","Koch Frost","Terrie Reyes","Robbie Willis","Minerva Anderson","Aguirre Aguilar","Sasha Bernard","Kirsten Mcgee","Bernadette Mcfarland","Pacheco Nieves","Huber Terry","Cote Finch","George Juarez","Beasley Rasmussen","Gabrielle Casey","Short Velez","Nora Brennan","Mendez Gaines","Sybil Cox","Sutton Robertson","Briggs Gregory","Green Marquez","Rachel Bradshaw","Sally Cummings","Lyons Navarro","Jolene Wilkins","Estelle Mclean","Lewis Huff","Kinney Hess","Catherine Morris","Newman Townsend","Natasha Pace","Erickson Kane","Susanne Mcconnell","Kristi Mack","Singleton Herring","Holt Kinney","Eloise Pugh","Wagner Ferrell","Alvarez Weaver","Emma Payne","Frost Wilkinson","Clarke Steele","Hall Sanford","Alberta Wiley","Durham Rivas","Rosemary Day","Terry Watson","Liza Camacho","Peck Travis","Mariana Blevins","Downs Thompson","Mercedes Valdez","Rodriquez Jennings","Hillary Fisher","Lana Fox","Ferguson Suarez","Renee Cervantes","Sparks Holder","Dorothea Clarke","Kristine Clemons","Daisy Dillard","Summers Yates","Hewitt Green","Burris Rose","Agnes Robles","Tammie Klein","Bernice Webster","Morgan Hart","Mable Reilly","Robles Whitehead","Gale Ortega","Maryellen Hartman","Raquel Solis","Mayo Faulkner","Duffy Copeland","Beck Atkins","Trina Padilla","Copeland Knapp","Valenzuela Chaney","Pena Schwartz","Martinez Kirby","Marks Acevedo","Erna Shelton","Solomon Pruitt","Elba Frank","Augusta Stark","Lidia Hale","Lang Herrera","Merritt Becker","Harris Hester","Mara Hansen","Claudine Mcdonald","Delaney Farmer","Valentine Rosales","Walton Horton","Essie Shepard","Hinton Goff","Duncan Quinn","Maria Ballard","Blanca Rosa","Barton Rush","Ada Palmer","Mamie Oliver","Addie Miles","Donaldson Harvey","Jaclyn Turner","Corine Taylor","Cora Gallegos","Eunice Glass","Walls Alexander","Shelton Barnett","Lee Price","Carla Davenport","Elinor Shepherd","Frederick Cash","Snow Guerrero","Duran Nguyen","Glenda Cantu","Samantha Jacobs","Tiffany Crane","Conway Monroe","Rich Ware","Haynes Carroll","Larsen Burris","Ella Walsh","Trujillo Guzman","Stafford Page"],
 
+    /**
+     * Object entry point.
+     * Should be called to configure all properties
+     */
     init: function() {
         this.buildTemplate();
     },
 
+    /**
+     * Data layer imitation
+     *
+     * @param amount
+     * @returns {Array}
+     */
     getPersons: function (amount) {
         var fixtures = [];
         var img = 1;
@@ -27,6 +37,16 @@ RG.helper = {
         return fixtures;
     },
 
+    /**
+     * This method parses the template and replaces tokens with values
+     * Returns string representation of the new node
+     *
+     * @param person
+     * @param offset
+     * @param displayed
+     *
+     * @returns {string}
+     */
     createTextNode: function (person, offset, displayed) {
         var imagePath = person.img + '?v=' + person.id;
         var fakeImage = 'public/img/0.jpeg';
@@ -53,6 +73,10 @@ RG.helper = {
         return textNode;
     },
 
+    /**
+     * Method for template initialization
+     * concat() function is used for better readability
+     */
     buildTemplate: function () {
         this.template = String().concat(
             '<div class="person" id="person-%%_id_%%" style="display: %%_display_%%; top: %%_top_%%px">',
